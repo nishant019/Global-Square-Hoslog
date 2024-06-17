2ca901028c1ae6a83cac3cc699bed555670bc7eb
nishant019 <npoudel018@outlook.com>
2024-06-17 15:21:41 +0545

    Gallery Fixes

--- cypress/e2e/Club v2/Host Management/host gallery management.cy.js
+++ cypress/e2e/Club v2/Host Management/host gallery management.cy.js
@@ -24,7 +24,7 @@ describe("Gallery management", () => {
         cy.viewport(1080, 720)
         cy.readXlsx(hostFilePath[0], hostFilePath[1], hostFilePath[2]).then(hostDetails => {
 
-            hostDetails.slice(0, 1).forEach(hostData => {
+            hostDetails.forEach(hostData => {
                 const imageLocation = hostData.GalleryImageList + '/' + clubName + '/' + hostData.hostName
 
                 if (hostData.GalleryImageList) {
@@ -32,28 +32,36 @@ describe("Gallery management", () => {
                     cy.get('.chakra-input').type(`${hostData.hostNameKatakana}{enter}`).wait(4000)
                     cy.get('[data-rbd-droppable-id="staff"] td').eq(2).click()
 
-
-                    cy.get(".css-1x2x8x9").click().wait(2000)
-
-
-
+                    // cy.get(".css-1x2x8x9").click().wait(2000)
 
                     cy.getFolderContents(imageLocation).then((files) => {
-                        if (files) {
-                            files.forEach((file, index) => {
 
-                                cy.get(".css-1x2x8x9").click({ force: true }).wait(1000)
-                                cy.get('.css-tjq8tt').contains(hostData.hostName).click();
-                                cy.get('.css-pcwo9u').contains(hostData.hostName).click()
 
-                                cy.get('.css-ygtapu').eq(index).click().wait(500)
-                                cy.get('.css-wjnxbe > .css-145ers9').click().wait(1000)
-                            })
+                            if (files) {
+                                cy.get('.css-9p04zo').eq(0).click()
+                                cy.get('.css-9p04zo').eq(0).click()
+        
+                                cy.get('[type="file"]').eq(0).selectFile(imageLocation + '/' + files[0],{force:true})
+                                cy.get('.css-39tpl5 > .chakra-button').click().wait(1000)
+                                cy.get('[type="file"]').eq(0).selectFile(imageLocation + '/' + files[0],{force:true})
+                                cy.get('.css-39tpl5 > .chakra-button').click().wait(1000)
+        
+                                cy.get(".css-1x2x8x9").click().wait(2000)
 
-                        }
+                                files.forEach((file, index) => {
+
+                                    cy.get(".css-1x2x8x9").click({ force: true }).wait(1000)
+                                    cy.get('.css-tjq8tt').contains(hostData.hostName).click();
+                                    cy.get('.css-pcwo9u').contains(hostData.hostName).click()
+
+                                    cy.get('.css-ygtapu').eq(index).click().wait(500)
+                                    cy.get('.css-wjnxbe > .css-145ers9').click().wait(1000)
+                                })
+
+                            }
                     });
 
-                    cy.get('[data-e2e="release"]').click().wait(1000)
+                    // cy.get('[data-e2e="release"]').click().wait(1000)
 
                 }
 
